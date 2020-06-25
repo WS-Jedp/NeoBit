12fa3459e205485b30a559425f54030e605bf529
Jedp <jedp082@gmail.com>
2020-06-25 12:47:28 -0500

    Webpack config done

---
+++ DesignSystem/webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.sass', '.scss'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ],
+      },
+      {
+        test: /\.(s[ac]ss|css)$/,
+        exclude: /node_modules/,
+        use:[
+          MiniCSSExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'resolve-url-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options:{ 
+              sourceMap: true,
+              config: {
+                path: 'postcss.config.js'
+              }
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+        ]
+      },
+      {
+        test: /\.(svg|png|jpg|jpeg|gif|woff)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              publicPath: '/assets/images',
+              outputPath: 'assets/images'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin(
+      {
+        inject: true,
+        template: './public/index.html',
+        filename: 'index.html'
+      }
+    ),
+    new MiniCSSExtractPlugin({
+      filename: 'styles/[name].css',
+      chunkFilename: 'styles/[id].css'
+    })
+  ],
+}
