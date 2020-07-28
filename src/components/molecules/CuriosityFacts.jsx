0db38e455eeeee930243143d435484d96f2af077
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-07-27 19:07:26 -0500

    hotfix/texts The text were corrected

--- src/components/molecules/CuriosityFacts.jsx
+++ src/components/molecules/CuriosityFacts.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { texts } from '../manifests/curiosityFactsManifest.json';
 
-const CurosityFacts = ({ title, description }) => {
+const useRandom = textArray => {
+  const randomNumber = Math.floor(Math.random() * textArray.length);
+  return textArray[randomNumber];
+}
+
+const CurosityFacts = ({ title }) => {
+  const text = useRandom(texts);
   return (
     <div className="curiosityFacts">
       <h1>{title || "Titulo"}</h1>
-      <p>{description || "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolore delectus libero repellendus cumque, ipsam incidunt animi quam impedit id possimus pariatur, et officiis labore non distinctio rem, magnam doloremque?"}</p>
+      <p>{text || "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolore delectus libero repellendus cumque, ipsam incidunt animi quam impedit id possimus pariatur, et officiis labore non distinctio rem, magnam doloremque?"}</p>
     </div>
   );
 };
